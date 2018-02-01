30e5c5ec799dfe3a90d1fb932c994d81cc044fcf
mpentler <tehhustler@hotmail.com>
2018-02-01 19:33:14 +0000

    more tidying of submissions

--- animalfacts.py
+++ animalfacts.py
@@ -1110,10 +1110,10 @@ ELAND_FACTS = (
     'The common eland (Taurotragus oryx), also known as the southern eland or eland antelope, is a savannah and plains antelope found in East and Southern Africa.',
     'Common elands form herds of up to 500 animals, but are not territorial.',
     'The common eland is used by humans for leather, meat, and rich, nutritious milk, and has been domesticated in many areas.',
-    "While the common eland's population is decreasing, it is classified as 'Least Concern' by the International Union for Conservation of Nature (IUCN).",
-    "Mainly a herbivore, the eland's diet is primarily grasses and leaves.",
-    "The name 'eland' is Dutch for 'elk' or 'moose'.",
-    "Eland herds are accompanied by a loud clicking sound that has been subject to considerable speculation. It is believed that the weight of the animal causes the two halves of its hooves to splay apart, and the clicking is the result of the hoof snapping together when the animal raises its leg.",
+    'While the common eland\'s population is decreasing, it is classified as \'Least Concern\' by the International Union for Conservation of Nature (IUCN).',
+    'Mainly a herbivore, the eland\'s diet is primarily grasses and leaves.',
+    'The name \'eland\' is Dutch for \'elk\' or \'moose\'.',
+    'Eland herds are accompanied by a loud clicking sound that has been subject to considerable speculation. It is believed that the weight of the animal causes the two halves of its hooves to splay apart, and the clicking is the result of the hoof snapping together when the animal raises its leg.',
     'Common elands are nomadic and crepuscular. They eat in the morning and evening, rest in shade when hot and remain in sunlight when cold.',
     )
 
@@ -1131,21 +1131,21 @@ ELEPHANT_FACTS = (
     'Elephants can swim – they use their trunk to breathe like a snorkel in deep water.',
     'Elephants are herbivores and can spend up to 16 hours days collecting leaves, twigs, bamboo and roots.',
     )
-    
+
 ELEPHANT_SHREW_FACTS = (
-	'Elephant shrews, or jumping shrews, are small insectivorous mammals native to Africa, belonging to the family Macroscelididae, in the order Macroscelidea,(there are 19 species of elephant shrew, placed in four genera) whose traditional common English name comes from resemblance between their long noses and the trunk of an elephant, and an assumed relationship with the shrews.',
-	'They are widely distributed across the southern part of Africa, and although common nowhere, can be found in almost any type of habitat, from the Namib Desert to boulder-strewn outcrops in South Africa to thick forest.',
-	'Elephant shrew is classified as endangered largely due to a fragmented forest environment and anthropogenic factors.',
-	'Despite their weight of under half a kilogram, they have been recorded to reach speeds of 28.8 km/h, making it one of the fastest small mammals.',
-	'Elephant shrews are small, quadrupedal, insectivorous mammals resembling rodents or opossums, with scaly tails, elongated snouts, and rather long legs for their size, which are used to move in a hopping fashion like rabbits.',
-	'They vary in size from about 10 cm to almost 30 cm, from just under 50 g to over 500 g.',
-	'The short-eared elephant shrew has an average size of 150 mm (5.9 in).',
-	'Their lifespans are about two and a half to four years in the wild.',
-	'They have large canine teeth, and also high-crowned cheek teeth similar to those of ungulates.',
-	'They are diurnal and very active.',
-	'They are not highly social animals, but many live in monogamous pairs.',
-	'Elephant shrews mainly eat insects, spiders, centipedes etc.',
-	)
+    'Elephant shrews, or jumping shrews, are small insectivorous mammals native to Africa, belonging to the family Macroscelididae, in the order Macroscelidea,(there are 19 species of elephant shrew, placed in four genera) whose traditional common English name comes from resemblance between their long noses and the trunk of an elephant, and an assumed relationship with the shrews.',
+    'They are widely distributed across the southern part of Africa, and although common nowhere, can be found in almost any type of habitat, from the Namib Desert to boulder-strewn outcrops in South Africa to thick forest.',
+    'Elephant shrew is classified as endangered largely due to a fragmented forest environment and anthropogenic factors.',
+    'Despite their weight of under half a kilogram, they have been recorded to reach speeds of 28.8 km/h, making it one of the fastest small mammals.',
+    'Elephant shrews are small, quadrupedal, insectivorous mammals resembling rodents or opossums, with scaly tails, elongated snouts, and rather long legs for their size, which are used to move in a hopping fashion like rabbits.',
+    'They vary in size from about 10 cm to almost 30 cm, from just under 50 g to over 500 g.',
+    'The short-eared elephant shrew has an average size of 150 mm (5.9 in).',
+    'Their lifespans are about two and a half to four years in the wild.',
+    'They have large canine teeth, and also high-crowned cheek teeth similar to those of ungulates.',
+    'They are diurnal and very active.',
+    'They are not highly social animals, but many live in monogamous pairs.',
+    'Elephant shrews mainly eat insects, spiders, centipedes etc.',
+    )
 
 ELK_FACTS = (
     'Elks are larger than a deer, but not as massive as moose.',
